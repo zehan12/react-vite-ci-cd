a6764e5e603469ce0a62d72195bf82700dec3c1b
Zehan Khan <zkhan@hootboard.com>
2023-07-30 00:50:17 +0530

    setup million with vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import AppBlock from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AppBlock />
   </React.StrictMode>,
 )
